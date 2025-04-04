c4cfb188aed609966e853dc1b8b8487a508bae9a
bhoomi <bhoomi.chakrani@hotmail.com>
2025-04-04 12:35:43 -0400

    Added stepdefinitions for linkedlist and stack

--- src/test/java/dsportal_stepdefinitions/LinkedListStepDefinition.java
+++ src/test/java/dsportal_stepdefinitions/LinkedListStepDefinition.java
@@ -1,5 +1,386 @@
 package dsportal_stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LinkedListStepDefinition {
 
+	@Given("The user is in the Home page after Sign in")
+	public void the_user_is_in_the_home_page_after_sign_in() {
+	    
+	    
+	}
+
+	@When("The user clicks the Get Started button in Linked List Panel \\/ The user select Linked List item from the drop down menu")
+	public void the_user_clicks_the_get_started_button_in_linked_list_panel_the_user_select_linked_list_item_from_the_drop_down_menu() {
+	    
+	    
+	}
+
+	@Then("The user be directed to Linked List Data Structure Page")
+	public void the_user_be_directed_to_linked_list_data_structure_page() {
+	    
+	    
+	}
+
+	@Given("The user is in the Stack page after Sign in")
+	public void the_user_is_in_the_stack_page_after_sign_in() {
+	    
+	    
+	}
+
+	@When("The user clicks Introduction in Linked List page")
+	public void the_user_clicks_introduction_in_linked_list_page() {
+	    
+	    
+	}
+
+	@Then("The user should be redirected to Introduction page")
+	public void the_user_should_be_redirected_to_introduction_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the Introduction in Stack page")
+	public void the_user_is_on_the_introduction_in_stack_page() {
+	    
+	    
+	}
+
+	@When("The user clicks Try Here>>> button in Introduction in Linked List page")
+	public void the_user_clicks_try_here_button_in_introduction_in_linked_list_page() {
+	    
+	    
+	}
+
+	@Then("Navigate to try Editor window with a Run button to test")
+	public void navigate_to_try_editor_window_with_a_run_button_to_test() {
+	    
+	    
+	}
+
+	@Given("The user is on the tryEditor Introduction page")
+	public void the_user_is_on_the_try_editor_introduction_page() {
+	    
+	    
+	}
+
+	@When("The user enters the Empty in text editor and clicks RUN Button")
+	public void the_user_enters_the_empty_in_text_editor_and_clicks_run_button() {
+	    
+	    
+	}
+
+	@Then("The Error message in alert window should be displayed")
+	public void the_error_message_in_alert_window_should_be_displayed() {
+	    
+	    
+	}
+
+	@When("The user enters the Valid code in text editor and clicks RUN Button")
+	public void the_user_enters_the_valid_code_in_text_editor_and_clicks_run_button() {
+	    
+	    
+	}
+
+	@Then("The displayed in the console should be displayed")
+	public void the_displayed_in_the_console_should_be_displayed() {
+	    
+	    
+	}
+
+	@When("The user enters the Invalid code in text editor and clicks RUN Button")
+	public void the_user_enters_the_invalid_code_in_text_editor_and_clicks_run_button() {
+	    
+	    
+	}
+
+	@Then("The nameerror pop up alert message should be displayed")
+	public void the_nameerror_pop_up_alert_message_should_be_displayed() {
+	    
+	    
+	}
+
+	@Given("The user is on the tryEditor Operations for Introduction page")
+	public void the_user_is_on_the_try_editor_operations_for_introduction_page() {
+	    
+	    
+	}
+
+	@When("The user clicks Practice Questions button following browser back arrow")
+	public void the_user_clicks_practice_questions_button_following_browser_back_arrow() {
+	    
+	    
+	}
+
+	@Then("The user is navigated to Practice Questions page")
+	public void the_user_is_navigated_to_practice_questions_page() {
+	    
+	    
+	}
+
+	@Given("The user is in the blank Practice page")
+	public void the_user_is_in_the_blank_practice_page() {
+	    
+	    
+	}
+
+	@When("The user select Linked List item from the drop down menu")
+	public void the_user_select_linked_list_item_from_the_drop_down_menu() {
+	    
+	    
+	}
+
+	@Then("The user is redirected to Linked List Data Structure Page")
+	public void the_user_is_redirected_to_linked_list_data_structure_page() {
+	    
+	    
+	}
+
+	@Given("The user is in the Linked LIst page after Sign in")
+	public void the_user_is_in_the_linked_l_ist_page_after_sign_in() {
+	    
+	    
+	}
+
+	@When("The user clicks Creating Linked LIst button")
+	public void the_user_clicks_creating_linked_l_ist_button() {
+	    
+	    
+	}
+
+	@Then("The user should be redirected to Creating Linked LIst page")
+	public void the_user_should_be_redirected_to_creating_linked_l_ist_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the Creating Linked LIst page")
+	public void the_user_is_on_the_creating_linked_l_ist_page() {
+	    
+	    
+	}
+
+	@When("The user clicks Try Here>>> button in Creating Linked LIst page")
+	public void the_user_clicks_try_here_button_in_creating_linked_l_ist_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the tryEditor Creating Linked LIst page")
+	public void the_user_is_on_the_try_editor_creating_linked_l_ist_page() {
+	    
+	    
+	}
+
+	@When("The user select Linked LIst item from the drop down menu")
+	public void the_user_select_linked_l_ist_item_from_the_drop_down_menu() {
+	    
+	    
+	}
+
+	@Then("The user is redirected to Linked LIst Data Structure Page")
+	public void the_user_is_redirected_to_linked_l_ist_data_structure_page() {
+	    
+	    
+	}
+
+	@Given("The user is in the Linked List page after Sign in")
+	public void the_user_is_in_the_linked_list_page_after_sign_in() {
+	    
+	    
+	}
+
+	@When("The user clicks Types of Linked List in Linked List Page")
+	public void the_user_clicks_types_of_linked_list_in_linked_list_page() {
+	    
+	    
+	}
+
+	@Then("The user should be redirected to Types of Linked List in Linked List page")
+	public void the_user_should_be_redirected_to_types_of_linked_list_in_linked_list_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the Types of Linked List page")
+	public void the_user_is_on_the_types_of_linked_list_page() {
+	    
+	    
+	}
+
+	@When("The user clicks Try Here>>> button in Types of Linked List page")
+	public void the_user_clicks_try_here_button_in_types_of_linked_list_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the tryEditor Types of Linked List page")
+	public void the_user_is_on_the_try_editor_types_of_linked_list_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the tryEditor Operations for Types of Linked List page")
+	public void the_user_is_on_the_try_editor_operations_for_types_of_linked_list_page() {
+	    
+	    
+	}
+
+	@When("The user clicks Implement Linked List in Python in Linked List Page")
+	public void the_user_clicks_implement_linked_list_in_python_in_linked_list_page() {
+	    
+	    
+	}
+
+	@Then("The user should be redirected to Implement Linked List in Python in Linked List page")
+	public void the_user_should_be_redirected_to_implement_linked_list_in_python_in_linked_list_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the Implement Linked List in Python page")
+	public void the_user_is_on_the_implement_linked_list_in_python_page() {
+	    
+	    
+	}
+
+	@When("The user clicks Try Here>>> button in Implement Linked List in Python page")
+	public void the_user_clicks_try_here_button_in_implement_linked_list_in_python_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the tryEditor Implement Linked List in Python page")
+	public void the_user_is_on_the_try_editor_implement_linked_list_in_python_page() {
+	    
+	    
+	}
+
+	@When("The user clicks Traversal in Linked List Page")
+	public void the_user_clicks_traversal_in_linked_list_page() {
+	    
+	    
+	}
+
+	@Then("The user should be redirected to Traversal in Linked List page")
+	public void the_user_should_be_redirected_to_traversal_in_linked_list_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the Traversal page")
+	public void the_user_is_on_the_traversal_page() {
+	    
+	    
+	}
+
+	@When("The user clicks Try Here>>> button in Traversal page")
+	public void the_user_clicks_try_here_button_in_traversal_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the tryEditor Traversal page")
+	public void the_user_is_on_the_try_editor_traversal_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the tryEditor Operations for Traversal page")
+	public void the_user_is_on_the_try_editor_operations_for_traversal_page() {
+	    
+	    
+	}
+
+	@When("The user clicks Insertion in Linked List Page")
+	public void the_user_clicks_insertion_in_linked_list_page() {
+	    
+	    
+	}
+
+	@Then("The user should be redirected to Insertion in Linked List page")
+	public void the_user_should_be_redirected_to_insertion_in_linked_list_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the Insertion page")
+	public void the_user_is_on_the_insertion_page() {
+	    
+	    
+	}
+
+	@When("The user clicks Try Here>>> button in Insertion page")
+	public void the_user_clicks_try_here_button_in_insertion_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the tryEditor Insertion page")
+	public void the_user_is_on_the_try_editor_insertion_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the tryEditor Operations for Insertion page")
+	public void the_user_is_on_the_try_editor_operations_for_insertion_page() {
+	    
+	    
+	}
+
+	@When("The user clicks Deletion in Linked List Page")
+	public void the_user_clicks_deletion_in_linked_list_page() {
+	    
+	    
+	}
+
+	@Then("The user should be redirected to Deletion in Linked List page")
+	public void the_user_should_be_redirected_to_deletion_in_linked_list_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the Deletion page")
+	public void the_user_is_on_the_deletion_page() {
+	    
+	    
+	}
+
+	@When("The user clicks Try Here>>> button in Deletion page")
+	public void the_user_clicks_try_here_button_in_deletion_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the tryEditor Deletion page")
+	public void the_user_is_on_the_try_editor_deletion_page() {
+	    
+	    
+	}
+
+	@Given("The user is on the tryEditor Operations for Deletion page")
+	public void the_user_is_on_the_try_editor_operations_for_deletion_page() {
+	    
+	    
+	}
+
+	@Given("The user is in the Linked List page")
+	public void the_user_is_in_the_linked_list_page() {
+	    
+	    
+	}
+
+	@When("The user clicks Sign out")
+	public void the_user_clicks_sign_out() {
+	    
+	    
+	}
+
+	@Then("The user should be redirected to home page with message Logged out successfully")
+	public void the_user_should_be_redirected_to_home_page_with_message_logged_out_successfully() {
+	    
+	    
+	}
 }

03a4e1d869311f45f5efaec4c2d8478d74780ed9
anbugit25 <anbunaga82@gmail.com>
2025-04-05 17:55:56 -0400

    files updated

---
+++ src/test/java/dsportal_stepdefinitions/GraphStepDefinition.java
@@ -0,0 +1,179 @@
+package dsportal_stepdefinitions;
+//import utilities.LoggerLoad;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GraphStepDefinition {
+	
+	
+	@Given("The user is in the Graph Introduction Page after Sign in")
+	public void the_user_is_in_the_graph_introduction_page_after_sign_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks the  Graph link in Graph Introduction page")
+	public void the_user_clicks_the_graph_link_in_graph_introduction_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user be directed to Graph Page")
+	public void the_user_be_directed_to_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is in the Graph page")
+	public void the_user_is_in_the_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user enters the Empty in Text Editor and clicks RUN button")
+	public void the_user_enters_the_empty_in_text_editor_and_clicks_run_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The Error message in alert window should be displayed")
+	public void the_error_message_in_alert_window_should_be_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user enters the valid code in Text Editor and clicks RUN button")
+	public void the_user_enters_the_valid_code_in_text_editor_and_clicks_run_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The displayed in the console should be displayed")
+	public void the_displayed_in_the_console_should_be_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user enters the Invalid code1 in Text Editor and clicks RUN button")
+	public void the_user_enters_the_invalid_code1_in_text_editor_and_clicks_run_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The nameerror pop up alert message should be displayed")
+	public void the_nameerror_pop_up_alert_message_should_be_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the Try Editor of Graph page")
+	public void the_user_is_on_the_try_editor_of_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks grabrowser back arrow")
+	public void the_user_clicks_grabrowser_back_arrow() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user is navigated to Graph page")
+	public void the_user_is_navigated_to_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks the Graph Representations link in Graph Introduction page")
+	public void the_user_clicks_the_graph_representations_link_in_graph_introduction_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user be directed to Graph Representations Page")
+	public void the_user_be_directed_to_graph_representations_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is in the Graph Representations page")
+	public void the_user_is_in_the_graph_representations_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the GrareptryEditor page")
+	public void the_user_is_on_the_grareptry_editor_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the Try Editor of Graph Representations page")
+	public void the_user_is_on_the_try_editor_of_graph_representations_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks grepbrowser back arrow")
+	public void the_user_clicks_grepbrowser_back_arrow() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user is navigated to Graph Representations page")
+	public void the_user_is_navigated_to_graph_representations_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("the user is signed in to dsAlgo Portal and is on the Graph page")
+	public void the_user_is_signed_in_to_ds_algo_portal_and_is_on_the_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user clicks the Practice Questions button")
+	public void the_user_clicks_the_practice_questions_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the user should be redirected to the Practice page")
+	public void the_user_should_be_redirected_to_the_practice_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user is redirected to Graph Page")
+	public void the_user_is_redirected_to_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user clicks Sign out")
+	public void the_user_clicks_sign_out() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the user is directed to Home page with message Logged out successfully")
+	public void the_user_is_directed_to_home_page_with_message_logged_out_successfully() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+	
+}

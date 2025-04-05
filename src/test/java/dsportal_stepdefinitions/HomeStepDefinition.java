1ccc438ae23b1407030b0f5d05fbaaeffa9f37c8
Nivetha-Ram <happymomenttome@gmail.com>
2025-04-05 17:22:30 -0400

    Second commit

--- src/test/java/dsportal_stepdefinitions/HomeStepDefinition.java
+++ src/test/java/dsportal_stepdefinitions/HomeStepDefinition.java
@@ -5,95 +5,237 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class HomeStepDefinition {
-	@Given("The user has browser open")
-	public void the_user_has_browser_open() {
+	@When("The user clicks the Get Started button")
+	public void the_user_clicks_the_get_started_button() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@When("The user enter correct dsAlgo portal URL")
-	public void the_user_enter_correct_ds_algo_portal_url() {
+	@Then("The user should be navigated to the Data Structure Introduction page, which displays the Register and Sign in links.")
+	public void the_user_should_be_navigated_to_the_data_structure_introduction_page_which_displays_the_register_and_sign_in_links() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@Then("The user should be able to land on dsAlgo portal with Get Started button")
-	public void the_user_should_be_able_to_land_on_ds_algo_portal_with_get_started_button() {
+	@Then("The user should able to see {int} options Arrays Linked,List,Stack,Queue,Tree,Graph in dropdown menu")
+	public void the_user_should_able_to_see_options_arrays_linked_list_stack_queue_tree_graph_in_dropdown_menu(Integer int1) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@Given("The user is on the DS Algo Portal")
-	public void the_user_is_on_the_ds_algo_portal() {
+	@When("The user selects Data Structures from the drop down without Sign in.")
+	public void the_user_selects_data_structures_from_the_drop_down_without_sign_in() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@When("The user clicks the {string} button")
-	public void the_user_clicks_the_button(String string) {
+	@Then("The user should able to see an warning message You are not logged in")
+	public void the_user_should_able_to_see_an_warning_message_you_are_not_logged_in() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@Then("The user should be navigated to the Data Structure Introduction page, which displays the {string} and {string} links.")
-	public void the_user_should_be_navigated_to_the_data_structure_introduction_page_which_displays_the_and_links(String string, String string2) {
+	@When("The user selects Arrays from the drop down without Sign in.")
+	public void the_user_selects_arrays_from_the_drop_down_without_sign_in() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@Given("The user is on the Home page")
-	public void the_user_is_on_the_home_page() {
+	@When("The user selects Linked List from the drop down without Sign in.")
+	public void the_user_selects_linked_list_from_the_drop_down_without_sign_in() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@When("The user clicks the Data Structures dropdown")
-	public void the_user_clicks_the_data_structures_dropdown() {
+	@When("The user selects Stack from the drop down without Sign in.")
+	public void the_user_selects_stack_from_the_drop_down_without_sign_in() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@Then("The user should able to see {int} options Arrays {string} in dropdown menu")
-	public void the_user_should_able_to_see_options_arrays_in_dropdown_menu(Integer int1, String string) {
+	@When("The user selects Queue from the drop down without Sign in.")
+	public void the_user_selects_queue_from_the_drop_down_without_sign_in() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@When("The user selects {string} from the drop down without Sign in.")
-	public void the_user_selects_from_the_drop_down_without_sign_in(String string) {
+	@When("The user selects Tree from the drop down without Sign in.")
+	public void the_user_selects_tree_from_the_drop_down_without_sign_in() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@Then("The user should able to see an warning message {string}")
-	public void the_user_should_able_to_see_an_warning_message(String string) {
+	@When("The user selects Graph from the drop down without Sign in.")
+	public void the_user_selects_graph_from_the_drop_down_without_sign_in() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@When("The user selects {string} from the drop down after Log in.")
-	public void the_user_selects_from_the_drop_down_after_log_in(String string) {
+	@When("The user selects Data Structures from the drop down after Log in.")
+	public void the_user_selects_data_structures_from_the_drop_down_after_log_in() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@Then("The user should be redirected to {string} page")
-	public void the_user_should_be_redirected_to_page(String string) {
+	@Then("The user should be redirected to Data Structures page")
+	public void the_user_should_be_redirected_to_data_structures_page() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@When("The user clicks {string} buttons of {string} on the homepage without Sign in")
-	public void the_user_clicks_buttons_of_on_the_homepage_without_sign_in(String string, String string2) {
+	@When("The user selects Array from the drop down after Log in.")
+	public void the_user_selects_array_from_the_drop_down_after_log_in() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@When("The user clicks {string} buttons of {string} on the homepage while Log in")
-	public void the_user_clicks_buttons_of_on_the_homepage_while_log_in(String string, String string2) {
+	@Then("The user should be redirected to Array page")
+	public void the_user_should_be_redirected_to_array_page() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
+	@When("The user selects Linked List from the drop down after Log in.")
+	public void the_user_selects_linked_list_from_the_drop_down_after_log_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 
+	@Then("The user should be redirected to Linked List page")
+	public void the_user_should_be_redirected_to_linked_list_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user selects Stack from the drop down after Log in.")
+	public void the_user_selects_stack_from_the_drop_down_after_log_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to Stack page")
+	public void the_user_should_be_redirected_to_stack_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user selects Queue from the drop down after Log in.")
+	public void the_user_selects_queue_from_the_drop_down_after_log_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to Queue page")
+	public void the_user_should_be_redirected_to_queue_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user selects Tree from the drop down after Log in.")
+	public void the_user_selects_tree_from_the_drop_down_after_log_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to Tree page")
+	public void the_user_should_be_redirected_to_tree_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user selects Graph from the drop down after Log in.")
+	public void the_user_selects_graph_from_the_drop_down_after_log_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to Graph page")
+	public void the_user_should_be_redirected_to_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Get Started buttons of Data Structures-Introduction on the homepage without Sign in")
+	public void the_user_clicks_get_started_buttons_of_data_structures_introduction_on_the_homepage_without_sign_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Get Started buttons of Array on the homepage without Sign in")
+	public void the_user_clicks_get_started_buttons_of_array_on_the_homepage_without_sign_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Get Started buttons of Linked List on the homepage without Sign in")
+	public void the_user_clicks_get_started_buttons_of_linked_list_on_the_homepage_without_sign_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Get Started buttons of Stack on the homepage without Sign in")
+	public void the_user_clicks_get_started_buttons_of_stack_on_the_homepage_without_sign_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Get Started buttons of Queue on the homepage without Sign in")
+	public void the_user_clicks_get_started_buttons_of_queue_on_the_homepage_without_sign_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Get Started buttons of Tree on the homepage without Sign in")
+	public void the_user_clicks_get_started_buttons_of_tree_on_the_homepage_without_sign_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Get Started buttons of Graph on the homepage without Sign in")
+	public void the_user_clicks_get_started_buttons_of_graph_on_the_homepage_without_sign_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Get Started buttons of Data Structures-Introduction on the homepage while Log in")
+	public void the_user_clicks_get_started_buttons_of_data_structures_introduction_on_the_homepage_while_log_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Get Started buttons of Array on the homepage while Log in")
+	public void the_user_clicks_get_started_buttons_of_array_on_the_homepage_while_log_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Get Started buttons of Linked List on the homepage while Log in")
+	public void the_user_clicks_get_started_buttons_of_linked_list_on_the_homepage_while_log_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Get Started buttons of Stack on the homepage while Log in")
+	public void the_user_clicks_get_started_buttons_of_stack_on_the_homepage_while_log_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Get Started buttons of Queue on the homepage while Log in")
+	public void the_user_clicks_get_started_buttons_of_queue_on_the_homepage_while_log_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Get Started buttons of Tree on the homepage while Log in")
+	public void the_user_clicks_get_started_buttons_of_tree_on_the_homepage_while_log_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Get Started buttons of Graph on the homepage while Log in")
+	public void the_user_clicks_get_started_buttons_of_graph_on_the_homepage_while_log_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 }
--- src/test/resources/features/Register.feature
+++ src/test/resources/features/Register.feature
@@ -23,32 +23,32 @@ Feature: Register page features
   @tag1
   Scenario: Verify that user is able to land on Register Page
     Given The user is on the user registration page
-    When The user clicks "Register" link on the Home page
+    When The user clicks Register link on the Home page
     Then The user should be redirected to Register form
     
    @tag2
   Scenario: Verify that user receives error message for all empty fields during registration
     Given The user is on the user registration page
     When The user clicks Register button with all fields empty
-    Then The error "Please fill out this field." appears below Username textbox
+    Then The error Please fill out this field. appears below Username textbox
     
    @tag3
   Scenario: Verify that user receives error message for empty Username field during registration
     Given The user is on the user registration page
     When The user clicks Register button without entering Username with all other fields filled
-    Then The error message "Please fill out this field." appears below Username textbox
+    Then The error message Please fill out this field. appears below Username textbox
     
    @tag4
   Scenario: Verify that user receives error message for empty Password field during registration
     Given The user is on the user registration page
     When The user clicks Register button after entering Username with other fields empty
-    Then The error message "Please fill out this field." appears below Password textbox
+    Then The error message Please fill out this field. appears below Password textbox
     
    @tag5
   Scenario: Verify that user receives error message for empty Password Confirmation field during registration
     Given The user is on the user registration page
     When The user clicks Register button after entering Username and Password with Password Confirmation field empty
-    Then The error message "Please fill out this field." appears below  Password Confirmation textbox
+    Then The error message Please fill out this field. appears below  Password Confirmation textbox
     
    @tag6
   Scenario: Verify that user receives error message for invalid username field during registration
@@ -65,8 +65,8 @@ Feature: Register page features
    @tag8
   Scenario: Verify that user receives error message for mismatched Password and Password Confirmation field during registration
     Given The user is on the user registration page
-    When The user clicks "Register" button after entering different passwords in Password and Password Confirmation fields
-    Then The user should able to see an pwd warning message "password_mismatch:The two password fields didn’t match."
+    When The user clicks Register button after entering different passwords in Password and Password Confirmation fields
+    Then The user should able to see an pwd warning message password_mismatch:The two password fields didn’t match.
     
    @tag9
   Scenario: Verify that user receives error message for existing Username during registration
@@ -84,7 +84,7 @@ Feature: Register page features
   Scenario: Verify that user is able to land on Homepage after registration with valid fields
     Given The user is on the user registration page
     When The user clicks Register button after entering  with valid username, password and password confirmation in related textboxes
-    Then The user should be redirected to Home Page of DS Algo with message "New Account Created. You are logged in as <ID>"
+    Then The user should be redirected to Home Page of DS Algo with message New Account Created. You are logged in as <ID>
     
     #@tagDemo
   #Scenario Outline: Successful user registration

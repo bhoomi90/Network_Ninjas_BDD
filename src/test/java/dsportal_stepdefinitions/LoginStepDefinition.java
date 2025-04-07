8f05021a3d1b608a0761c4dc8b2ba4250bcfbfae
Nivetha-Ram <happymomenttome@gmail.com>
2025-04-06 21:30:44 -0400

    Modified features

--- src/test/java/dsportal_stepdefinitions/LoginStepDefinition.java
+++ src/test/java/dsportal_stepdefinitions/LoginStepDefinition.java
@@ -5,42 +5,81 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginStepDefinition {
-	@When("The user clicks login button after leaving the Username textbox and Password textbox empty")
-	public void the_user_clicks_login_button_after_leaving_the_username_textbox_and_password_textbox_empty() {
+	@Given("The user is on the DS Algo Home Page")
+	public void the_user_is_on_the_ds_algo_home_page() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@Then("The error message Please fill out this field. appears below Username textbox")
-	public void the_error_message_please_fill_out_this_field_appears_below_username_textbox() {
+	@When("The user should click the Sign in link")
+	public void the_user_should_click_the_sign_in_link() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@When("The user clicks login button after entering the Username and leaves Password textbox empty")
-	public void the_user_clicks_login_button_after_entering_the_username_and_leaves_password_textbox_empty() {
+	@Then("The user should be redirected to Sign in page")
+	public void the_user_should_be_redirected_to_sign_in_page() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@Then("The error message Please fill out this field. appears below Password textbox")
-	public void the_error_message_please_fill_out_this_field_appears_below_password_textbox() {
+	@Given("The user is on the DS Algo Sign in Page")
+	public void the_user_is_on_the_ds_algo_sign_in_page() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@Then("The user should able to see an error message Invalid username and password.")
-	public void the_user_should_able_to_see_an_error_message_invalid_username_and_password() {
+	@When("The user clicks login button after leaving the {string} textbox and {string} textbox empty")
+	public void the_user_clicks_login_button_after_leaving_the_textbox_and_textbox_empty(String string, String string2) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@Then("The user should land in Data Structure Home Page with message You are logged in")
-	public void the_user_should_land_in_data_structure_home_page_with_message_you_are_logged_in() {
+	@When("The user clicks login button after entering the {string} and leaves {string} textbox empty")
+	public void the_user_clicks_login_button_after_entering_the_and_leaves_textbox_empty(String string, String string2) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 
+	@When("The user clicks login button after entering the Password only")
+	public void the_user_clicks_login_button_after_entering_the_password_only() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 
+	@When("The user clicks login button after entering invalid username")
+	public void the_user_clicks_login_button_after_entering_invalid_username() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 
+	@Then("The user should able to see an error message {string}.")
+	public void the_user_should_able_to_see_an_error_message(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks login button after entering valid username invalid password")
+	public void the_user_clicks_login_button_after_entering_valid_username_invalid_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks login button after entering valid username and valid password")
+	public void the_user_clicks_login_button_after_entering_valid_username_and_valid_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should land in Data Structure Home Page with message {string}")
+	public void the_user_should_land_in_data_structure_home_page_with_message(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is in the Home page after Sign out")
+	public void the_user_is_in_the_home_page_after_sign_out() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 }

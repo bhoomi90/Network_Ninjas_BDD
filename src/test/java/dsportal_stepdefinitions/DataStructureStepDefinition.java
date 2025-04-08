1baedb4a97e9a3b966632835a68281cedd2fa246
Nivetha-Ram <happymomenttome@gmail.com>
2025-04-07 22:11:04 -0400

    Edited features

--- src/test/java/dsportal_stepdefinitions/DataStructureStepDefinition.java
+++ src/test/java/dsportal_stepdefinitions/DataStructureStepDefinition.java
@@ -7,122 +7,122 @@ import io.cucumber.java.en.When;
 public class DataStructureStepDefinition {
 	@Given("The user is in the Home page after logged in")
 	public void the_user_is_in_the_home_page_after_logged_in() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
+	   
 	}
 
-	@When("The user clicks the {string} button in Data Structures - Introduction OR The user select Data Structures - Introduction item from the drop down menu")
-	public void the_user_clicks_the_button_in_data_structures_introduction_or_the_user_select_data_structures_introduction_item_from_the_drop_down_menu(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@When("The user clicks the Getting Started button in Data Structures - Introduction OR The user select Data Structures - Introduction item from the drop down menu")
+	public void the_user_clicks_the_getting_started_button_in_data_structures_introduction_or_the_user_select_data_structures_introduction_item_from_the_drop_down_menu() {
+	    
+	   
 	}
 
-	@Then("The user be directed to {string}")
-	public void the_user_be_directed_to(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@Then("The user be directed to Data Structures- Introduction Page")
+	public void the_user_be_directed_to_data_structures_introduction_page() {
+	    
+	   
 	}
 
-	@Given("The user is in the {string} page")
-	public void the_user_is_in_the_page(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@Given("The user is in the Data Structures - Introduction page")
+	public void the_user_is_in_the_data_structures_introduction_page() {
+	    
+	   
 	}
 
-	@When("The user clicks {string} button")
-	public void the_user_clicks_button(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@When("The user clicks Time Complexity button")
+	public void the_user_clicks_time_complexity_button() {
+	    
+	   
 	}
 
-	@Then("The user should be redirected to {string} of Data structures-Introduction")
-	public void the_user_should_be_redirected_to_of_data_structures_introduction(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@Then("The user should be redirected to Time Complexity of Data structures-Introduction")
+	public void the_user_should_be_redirected_to_time_complexity_of_data_structures_introduction() {
+	    
+	   
+	}
+
+	@Given("The user is in the Time Complexity page")
+	public void the_user_is_in_the_time_complexity_page() {
+	    
+	   
+	}
+
+	@When("The user clicks Try Here button")
+	public void the_user_clicks_try_here_button() {
+	    
+	   
 	}
 
 	@Then("The user should be redirected to a page having an try Editor with a Run button to test")
 	public void the_user_should_be_redirected_to_a_page_having_an_try_editor_with_a_run_button_to_test() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
+	   
 	}
 
 	@Given("The user is in the tryEditor page")
 	public void the_user_is_in_the_try_editor_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
+	   
 	}
 
 	@When("The user clicks the Run Button without entering the code in the Editor")
 	public void the_user_clicks_the_run_button_without_entering_the_code_in_the_editor() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
+	   
 	}
 
 	@Then("The user should able to see an error message in alert window")
 	public void the_user_should_able_to_see_an_error_message_in_alert_window() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
+	   
 	}
 
 	@When("The user write the invalid code in Editor and click the Run Button")
 	public void the_user_write_the_invalid_code_in_editor_and_click_the_run_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
+	   
 	}
 
 	@When("The user write the valid code in Editor and click the Run Button")
 	public void the_user_write_the_valid_code_in_editor_and_click_the_run_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
+	   
 	}
 
 	@Then("The user should able to see output in the console")
 	public void the_user_should_able_to_see_output_in_the_console() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
+	   
 	}
 
-	@When("The user clicks the {string} button")
-	public void the_user_clicks_the_button(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@When("The user clicks the Practice Questions button")
+	public void the_user_clicks_the_practice_questions_button() {
+	    
+	   
 	}
 
-	@Given("The user is in the blank {string} page")
-	public void the_user_is_in_the_blank_page(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@Then("The user should be redirected to Practice Questions of Data structures-Introduction")
+	public void the_user_should_be_redirected_to_practice_questions_of_data_structures_introduction() {
+	    
+	   
 	}
 
 	@When("The user selected item from the drop down menu")
 	public void the_user_selected_item_from_the_drop_down_menu() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
+	   
 	}
 
-	@Then("The user be directed to \"selected item\"Page")
+	@Then("The user be directed to selected itemPage")
 	public void the_user_be_directed_to_selected_item_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
+	   
 	}
 
 	@Given("The user is in the Graph page")
 	public void the_user_is_in_the_graph_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("The user clicks {string}")
-	public void the_user_clicks(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Then("The user should be redirected to home page with message {string}")
-	public void the_user_should_be_redirected_to_home_page_with_message(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
+	   
 	}
 
 }

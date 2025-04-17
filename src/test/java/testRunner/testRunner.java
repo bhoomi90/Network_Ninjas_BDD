7344fb76727c2342f96d3dd2a06bd8fe81afd63f
Haritha <harithareddy2814@gmail.com>
2025-04-17 01:40:05 -0400

    | Added Cucumber options for Allure Test in test runner|

--- src/test/java/testRunner/testRunner.java
+++ src/test/java/testRunner/testRunner.java
@@ -1,5 +1,16 @@
 package testRunner;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features", glue = "dsportal_stepdtefinitions", plugin = { "pretty",
+		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" }
+
+)
 public class testRunner {
+	// No need for a main method; execution is handled by JUnit.
 
 }

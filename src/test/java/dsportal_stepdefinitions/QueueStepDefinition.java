8890cdd3fd5f3b81dc08471f519d4e6cfb7f0ff6
Haritha <harithareddy2814@gmail.com>
2025-04-17 01:45:24 -0400

    |Added and modified Feature, POM and StepDef files of Queue  |

--- src/test/java/dsportal_stepdefinitions/QueueStepDefinition.java
+++ src/test/java/dsportal_stepdefinitions/QueueStepDefinition.java
@@ -1,121 +1,163 @@
 package dsportal_stepdefinitions;
 
+import org.openqa.selenium.WebDriver;
+
+import dsportal_DriverFactory.PageDriverFactory;
+import dsportal_Page.QueuePage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class QueueStepDefinition {
-	@Given("The user is in the Home page after Sign in")
-	public void the_user_is_in_the_home_page_after_sign_in() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	static WebDriver driver;
+	static QueuePage queue;
+
+	public static void setUpQueuePage() {
+		try {
+			QueueStepDefinition.driver = PageDriverFactory.driver;
+			queue = new QueuePage(driver);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	@When("The user clicks the Get Started button in Queue Panel \\/ The user select Queue item from the drop down menu")
-	public void the_user_clicks_the_get_started_button_in_queue_panel_the_user_select_queue_item_from_the_drop_down_menu() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	@When("The user clicks the Get Started button in Queue Panel or The user select Queue item from the drop down menu")
+	public void the_user_clicks_the_get_started_button_in_queue_panel_or_the_user_select_queue_item_from_the_drop_down_menu() {
+		if (queue == null) {
+			setUpQueuePage();
+		}
+		// queue.clickGetStarted();
+		queue.dropdown_queue_page();
 	}
 
-	@Then("The user be directed to {string} Data Structure Page")
-	public void the_user_be_directed_to_data_structure_page(String string) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	@Then("The user be directed to Queue Data Structure Page")
+	public void the_user_be_directed_to_queue_data_structure_page() {
+
 	}
 
-	@Given("The user is in the {string} page after Sign in")
-	public void the_user_is_in_the_page_after_sign_in(String string) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
-
-	@When("The user clicks {string} button")
-	public void the_user_clicks_button(String string) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
-
-	@Then("The user should be redirected to {string} page")
-	public void the_user_should_be_redirected_to_page(String string) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
-
-	@Given("The user is on the {string} page")
-	public void the_user_is_on_the_page(String string) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
-
-	@When("The user clicks {string} button in Introduction page")
-	public void the_user_clicks_button_in_introduction_page(String string) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
-
-	@Then("The user should be redirected to a page having an try Editor with a Run button to test")
-	public void the_user_should_be_redirected_to_a_page_having_an_try_editor_with_a_run_button_to_test() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
-
-	@Given("The user is in the tryEditor page")
-	public void the_user_is_in_the_try_editor_page() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
-
-	@When("The user clicks the Run Button without entering the code in the Editor")
-	public void the_user_clicks_the_run_button_without_entering_the_code_in_the_editor() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
-
-	@Then("The user should able to see an error message in alert window")
-	public void the_user_should_able_to_see_an_error_message_in_alert_window() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
-
-	@When("The user write the invalid code in Editor and click the Run Button")
-	public void the_user_write_the_invalid_code_in_editor_and_click_the_run_button() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
-
-	@When("The user write the valid code in Editor and click the Run Button")
-	public void the_user_write_the_valid_code_in_editor_and_click_the_run_button() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
-
-	@Then("The user should able to see output in the console")
-	public void the_user_should_able_to_see_output_in_the_console() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
-
-	@Given("The user is in the blank {string} page")
-	public void the_user_is_in_the_blank_page(String string) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	@Given("The user is in the Queue page after Sign in")
+	public void the_user_is_in_the_queue_page_after_sign_in() {
 	}
 
 	@When("The user select Queue item from the drop down menu")
-	public void the_user_select_queue_item_from_the_drop_down_menu() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	public void the_user_select_Queue_item_from_the_drop_down_menu() {
+		queue.dropdown_queue_page();
+
 	}
 
-	@When("The user clicks {string} button in Implementation using collections.deque page")
-	public void the_user_clicks_button_in_implementation_using_collections_deque_page(String string) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	@When("The user clicks Implementation of Queue in Python button")
+	public void the_user_clicks_implementation_of_queue_in_python_button() {
+		queue.clickImplementationQueuePython();
 	}
 
-	@When("The user clicks {string} button in Queue Operations page")
-	public void the_user_clicks_button_in_queue_operations_page(String string) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	@Then("The user should be redirected to Implementation of Queue in Python page")
+	public void the_user_should_be_redirected_to_implementation_of_queue_in_python_page() {
+
+	}
+
+	@Given("The user is on the Implementation of Queue in Python page")
+	public void the_user_is_on_the_implementation_of_queue_in_python_page() {
+
+	}
+
+	@When("The user clicks Try Here>>> button in Queue Operations page")
+	public void the_user_clicks_try_here_button_in_queue_operations_page() {
+
+	}
+
+	@Given("The user is in the tryEditor Implementation of Queue in Python page")
+	public void the_user_is_in_the_try_editor_implementation_of_queue_in_python_page() {
+
+	}
+
+	@Given("The user is on the tryEditor Operations for Implementation of Queue in Python page")
+	public void the_user_is_on_the_try_editor_operations_for_implementation_of_queue_in_python_page() {
+
+	}
+
+	@When("The user clicks Implementation using collections.deque button")
+	public void the_user_clicks_implementation_using_collections_deque_button() {
+		queue.clickImplementationUsingCollectionsDeque();
+	}
+
+	@Then("The user should be redirected to Implementation using collections.deque page")
+	public void the_user_should_be_redirected_to_implementation_using_collections_deque_page() {
+
+	}
+
+	@Given("The user is on the Implementation using collections.deque page")
+	public void the_user_is_on_the_implementation_using_collections_deque_page() {
+
+	}
+
+	@Given("The user is on the tryEditor Implementation using collections.deque page")
+	public void the_user_is_on_the_try_editor_implementation_using_collections_deque_page() {
+
+	}
+
+	@Given("The user is on the tryEditor Operations for Implementation using collections.deque page")
+	public void the_user_is_on_the_try_editor_operations_for_implementation_using_collections_deque_page() {
+
+	}
+
+	@When("The user clicks Implementation using array button")
+	public void the_user_clicks_implementation_using_array_button() {
+		queue.clickImplementationUingArray();
+	}
+
+	@Then("The user should be redirected to Implementation using array page")
+	public void the_user_should_be_redirected_to_implementation_using_array_page() {
+
+	}
+
+	@Given("The user is on the Implementation using array page")
+	public void the_user_is_on_the_implementation_using_array_page() {
+
+	}
+
+	@Given("The user is on the tryEditor Implementation using array page")
+	public void the_user_is_on_the_try_editor_implementation_using_array_page() {
+
+	}
+
+	@Given("The user is on the tryEditor Operations for Implementation using array page")
+	public void the_user_is_on_the_try_editor_operations_for_implementation_using_array_page() {
+
+	}
+
+	@When("The user clicks Queue Operations button")
+	public void the_user_clicks_queue_operations_button() {
+		queue.clickQueueOperations();
+	}
+
+	@Then("The user should be redirected to Queue Operations page")
+	public void the_user_should_be_redirected_to_queue_operations_page() {
+
+	}
+
+	@Given("The user is on the Queue Operations page")
+	public void the_user_is_on_the_queue_operations_page() {
+
+	}
+
+	@Given("The user is in the tryEditor Queue Operations page")
+	public void the_user_is_in_the_try_editor_queue_operations_page() {
+
+	}
+
+	@Given("The user is on the tryEditor Operations for Queue Operations page")
+	public void the_user_is_on_the_try_editor_operations_for_queue_operations_page() {
+
+	}
+
+	@Then("The user is redirected to Queue Data Structure Page")
+	public void the_user_is_redirected_to_queue_data_structure_page() {
+
+	}
+
+	@Given("The user is in the Queue page")
+	public void the_user_is_in_the_Queue_page() {
+
 	}
 }

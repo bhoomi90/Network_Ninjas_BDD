b56ecc63bda0d565260c00090cb8d093f46fb420
Haritha <harithareddy2814@gmail.com>
2025-04-21 08:54:29 -0400

    |Graph Data Structure Related Changes |

--- src/test/java/dsportal_Page/GraphPage.java
+++ src/test/java/dsportal_Page/GraphPage.java
@@ -1,5 +1,106 @@
 package dsportal_Page;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import dsportal_utilities.LoggerReader;
+
 public class GraphPage {
+	public static WebDriver driver;
+	String URL = "https://dsportalapp.herokuapp.com/";
+
+	@FindBy(id = "id_username")
+	WebElement userName;
+
+	@FindBy(id = "id_password")
+	WebElement password;
+	@FindBy(xpath = ("//*[@data-toggle='dropdown']"))
+	WebElement dropdown;
+	@FindBy(xpath = ("//a[text()='Graph']"))
+	WebElement dropdowngraph;
+	@FindBy(xpath = ("//*[text()='Get Started']"))
+	WebElement getStarted;
+
+	@FindBy(xpath = ("//h5[text()='Graph']/../a[text()='Get Started']"))
+	WebElement graphIntro;
+	@FindBy(xpath = "//*[@type='submit']")
+	WebElement loginButton;
+
+	@FindBy(xpath = "//a[text()='Graph Representations']")
+	WebElement graphRep;
+
+	@FindBy(xpath = "//a[@class='list-group-item'][text()='Graph']")
+	WebElement graphPage;
+
+	@FindBy(xpath = "//a[text()='Try here>>>']")
+	WebElement tryEditorButton;
+
+	public GraphPage(WebDriver driver) {
+		GraphPage.driver = driver;
+		if(driver.getTitle().equals("NumpyNinja")) 
+			 LoggerReader.info("I am on ds-algo App");
+		 else
+			 throw new IllegalStateException("This is not ds-algo app. The current page is: " +driver.getCurrentUrl());
+		PageFactory.initElements(driver, this);
+	}
+	
+	 
+
+	public void GraphIntro() {
+		graphIntro.click();
+		LoggerReader.info("");
+	}
+
+	public void dropdown_graph_page() {
+		dropdown.click();
+		dropdowngraph.click();
+		LoggerReader.info("I am on Graph Page");
+	}
+
+	public void GraphRepresentations() {
+		graphRep.click();
+		LoggerReader.info("");
+	}
+
+	public void GraphElementPage() {
+		graphPage.click();
+		LoggerReader.info("");
+	}
+
+	public void clickTryEditor() {
+		tryEditorButton.click();
+		LoggerReader.info("");
+	}
+
+	public static void main(String[] args) {
+		driver = new EdgeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+
+		GraphPage obj = new GraphPage(driver);
+		obj.clickGetStarted();
+
+	}
+
+	private void clickGetStarted() {
+		getStarted.click();
+		LoggerReader.info("");
+	}
+
+	// Methods
+
+	public void clickLoginIn() {
+
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://dsportalapp.herokuapp.com/login");
+		userName.sendKeys("Network_Ninjas");
+		password.sendKeys("OrangeS@12");
+		loginButton.click();
+	}
 
 }
--- src/test/java/dsportal_stepdefinitions/GraphStepDefinition.java
+++ src/test/java/dsportal_stepdefinitions/GraphStepDefinition.java
@@ -1,172 +1,160 @@
 package dsportal_stepdefinitions;
 
+import org.openqa.selenium.WebDriver;
+
+import dsportal_DriverFactory.PageDriverFactory;
+import dsportal_Page.GraphPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class GraphStepDefinition {
+	static WebDriver driver;
+	static GraphPage graph;
+
+	public static void setUpGraphPage() {
+		try {
+			GraphStepDefinition.driver = PageDriverFactory.driver;
+			graph = new GraphPage(driver);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	@When("The user clicks the Get Started button in Graph Panel or The user select Graph item from the drop down menu")
+	public void the_user_clicks_the_get_started_button_in_Graph_panel_or_the_user_select_graph_item_from_the_drop_down_menu() {
+		if (graph == null) {
+			setUpGraphPage();
+		}
+//    graph.GraphIntro();
+		graph.dropdown_graph_page();
+	}
 
 	@Given("The user is in the Graph Introduction Page after Sign in")
 	public void the_user_is_in_the_graph_introduction_page_after_sign_in() {
-	   
-	    
+
 	}
 
-	@When("The user clicks the  Graph link in Graph Introduction page")
+	@When("The user select Graph item from the drop down menu")
+	public void the_user_select_graph_item_from_the_drop_down_menu() {
+		graph.dropdown_graph_page();
+
+	}
+	@Then("The user is redirected to Graph Data Structure Page")
+	public void the_user_is_redirected_to_graph_data_structure_page() {
+		
+	}
+
+	@When("The user clicks the Graph link in Graph Introduction page")
 	public void the_user_clicks_the_graph_link_in_graph_introduction_page() {
-	   
-	    
+
+		graph.GraphElementPage();
+
 	}
 
 	@Then("The user be directed to Graph Page")
 	public void the_user_be_directed_to_graph_page() {
-	   
-	    
+
+	}
+
+	@Then("The user be directed to Graph Data Structure Page")
+	public void the_user_be_directed_to_graph_data_strucuture_page() {
+
 	}
 
 	@Given("The user is in the Graph page")
 	public void the_user_is_in_the_graph_page() {
-	   
-	    
+
 	}
 
-	@When("The user enters the Empty in Text Editor and clicks RUN button")
-	public void the_user_enters_the_empty_in_text_editor_and_clicks_run_button() {
-	   
-	    
+	@Given("The user is in the Try Editor of Graph page")
+	public void the_user_is_in_the_try_editor_of_graph_page() {
+
 	}
 
-	@Then("The Error message in alert window should be displayed")
-	public void the_error_message_in_alert_window_should_be_displayed() {
-	   
-	    
-	}
+	@Given("The user is on the tryEditor Graph page")
+	public void the_user_is_on_the_try_editor_graph_page() {
 
-	@When("The user enters the valid code in Text Editor and clicks RUN button")
-	public void the_user_enters_the_valid_code_in_text_editor_and_clicks_run_button() {
-	   
-	    
-	}
-
-	@Then("The displayed in the console should be displayed")
-	public void the_displayed_in_the_console_should_be_displayed() {
-	   
-	    
-	}
-
-	@When("The user enters the Invalid code in Text Editor and clicks RUN button")
-	public void the_user_enters_the_invalid_code_in_text_editor_and_clicks_run_button() {
-	   
-	    
-	}
-
-	@Then("The nameerror pop up alert message should be displayed")
-	public void the_nameerror_pop_up_alert_message_should_be_displayed() {
-	   
-	    
-	}
-
-	@Given("The user is on the Try Editor of Graph page")
-	public void the_user_is_on_the_try_editor_of_graph_page() {
-	   
-	    
-	}
-
-	@When("The user clicks grabrowser back arrow")
-	public void the_user_clicks_grabrowser_back_arrow() {
-	   
-	    
 	}
 
 	@Then("The user is navigated to Graph page")
 	public void the_user_is_navigated_to_graph_page() {
-	   
-	    
+
 	}
 
 	@When("The user clicks the Graph Representations link in Graph Introduction page")
 	public void the_user_clicks_the_graph_representations_link_in_graph_introduction_page() {
-	   
-	    
+
+		graph.GraphRepresentations();
 	}
 
 	@Then("The user be directed to Graph Representations Page")
 	public void the_user_be_directed_to_graph_representations_page() {
-	   
+
+	}
+	@Given("The user is on the tryEditor Operations for Graph page")
+	public void the_user_is_on_the_try_editor_operations_for_graph_page() {
+	    
 	    
 	}
-
 	@Given("The user is in the Graph Representations page")
 	public void the_user_is_in_the_graph_representations_page() {
-	   
-	    
+
 	}
 
-	@Given("The user is on the GrareptryEditor page")
-	public void the_user_is_on_the_grareptry_editor_page() {
-	   
-	    
+	@Given("The user is on the tryEditor Graph Representations page")
+	public void the_user_is_on_the_try_editor_graph_representations_page() {
+
 	}
 
 	@Given("The user is on the Try Editor of Graph Representations page")
 	public void the_user_is_on_the_try_editor_of_graph_representations_page() {
-	   
-	    
-	}
 
-	@When("The user clicks grepbrowser back arrow")
-	public void the_user_clicks_grepbrowser_back_arrow() {
-	   
-	    
 	}
 
 	@Then("The user is navigated to Graph Representations page")
 	public void the_user_is_navigated_to_graph_representations_page() {
-	   
+
+	}
+
+	@Given("The user is on the tryEditor Operations for Graph Representations page")
+	public void the_user_is_on_the_try_editor_operations_for_graph_representations_page() {
+	    
 	    
 	}
 
+//	@Given("The user is in the Graph page")
+//	public void the_user_is_in_the_graph_page() {
+//	    
+//	    
+//	}
 	@Given("the user is signed in to dsAlgo Portal and is on the Graph page")
 	public void the_user_is_signed_in_to_ds_algo_portal_and_is_on_the_graph_page() {
-	   
-	    
+
 	}
 
 	@When("the user clicks the Practice Questions button")
 	public void the_user_clicks_the_practice_questions_button() {
-	   
-	    
+
 	}
 
+//
 	@Then("the user should be redirected to the Practice page")
 	public void the_user_should_be_redirected_to_the_practice_page() {
-	   
-	    
+
 	}
 
+//
 	@Then("The user is redirected to Graph Page")
 	public void the_user_is_redirected_to_graph_page() {
-	   
-	    
+
 	}
 
-	@When("the user clicks Sign out")
-	public void the_user_clicks_sign_out() {
-	   
-	    
-	}
-
-	@Then("the user is directed to Home page with message Logged out successfully")
+	@Then("The user is directed to Home page with message Logged out successfully")
 	public void the_user_is_directed_to_home_page_with_message_logged_out_successfully() {
 	   
 	    
 	}
-
-
-
-
-
-
-
 
 }

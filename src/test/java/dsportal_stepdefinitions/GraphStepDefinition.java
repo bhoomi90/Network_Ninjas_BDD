ba86f3eb312beab154e92e05322daf528fff7449
bhoomi <bhoomi.chakrani@hotmail.com>
2025-04-28 16:56:56 -0400

    Fix Graph panel

--- src/test/java/dsportal_stepdefinitions/GraphStepDefinition.java
+++ src/test/java/dsportal_stepdefinitions/GraphStepDefinition.java
@@ -1,172 +1,109 @@
 package dsportal_stepdefinitions;
 
+import org.testng.Assert;
+
+import dsportal_DriverFactory.DriverManager;
+import dsportal_Page.GraphPage;
+import dsportal_Page.LoginPage;
+import dsportal_utilities.LoggerReader;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class GraphStepDefinition {
+public class GraphStepDefinition extends DriverManager  {
+	
+	static GraphPage graph;
+	static LoginPage lp;
+	String pageTitle, currentPageUrl, expectedPageUrl ;
+	String homePageURL = "https://dsportalapp.herokuapp.com/";
+	String GraphPageURL = homePageURL+"graph/";
+
+	public static void setUpGraphPage() {
+		graph = new GraphPage(driver);
+	}
+	
+	@When("The user clicks the Get Started button in Graph Panel or The user select Graph item from the drop down menu")
+	public void the_user_clicks_the_get_started_button_in_graph_panel_or_the_user_select_graph_item_from_the_drop_down_menu() {
+		if (graph == null) {
+			LoggerReader.info("Create constructor for Graph Page");
+			setUpGraphPage();
+		}
+		graph.dropdown_graph_page();
+	}
+
+	@Then("The user is directed to Graph Data Structure Page")
+	public void the_user_is_directed_to_graph_data_structure_page() {
+		   currentPageUrl = graph.getCurrentUrl();
+		   expectedPageUrl = GraphPageURL;
+		   LoggerReader.info("The user is able to directed to graph page: "+currentPageUrl);
+		   Assert.assertEquals(expectedPageUrl, currentPageUrl,"The user is not directed to graph page");    
+	}
 
 	@Given("The user is in the Graph Introduction Page after Sign in")
 	public void the_user_is_in_the_graph_introduction_page_after_sign_in() {
-	   
+		pageTitle =graph.getCurrentTitle();
+		Assert.assertEquals(pageTitle, "Graph");
+
 	    
 	}
 
-	@When("The user clicks the  Graph link in Graph Introduction page")
+	@When("The user clicks the Graph link in Graph Introduction page")
 	public void the_user_clicks_the_graph_link_in_graph_introduction_page() {
-	   
+		graph.GraphElementPage();
 	    
 	}
 
 	@Then("The user be directed to Graph Page")
 	public void the_user_be_directed_to_graph_page() {
-	   
+		   currentPageUrl = graph.getCurrentUrl();
+		   expectedPageUrl = "https://dsportalapp.herokuapp.com/graph/graph/";
+		   LoggerReader.info("The user is able to directed to graph page: "+currentPageUrl);
+		   Assert.assertEquals(expectedPageUrl, currentPageUrl,"The user is not directed to graph page");    	    
 	    
 	}
 
 	@Given("The user is in the Graph page")
 	public void the_user_is_in_the_graph_page() {
-	   
+		pageTitle =graph.getCurrentTitle();
+		Assert.assertEquals(pageTitle, "Graph");
+
 	    
 	}
 
-//	@When("The user enters the Empty in Text Editor and clicks RUN button")
-//	public void the_user_enters_the_empty_in_text_editor_and_clicks_run_button() {
-//	   
-//	    
-//	}
-
-//	@Then("The Error message in alert window should be displayed")
-//	public void the_error_message_in_alert_window_should_be_displayed() {
-//	   
-//	    
-//	}
-
-//	@When("The user enters the valid code in Text Editor and clicks RUN button")
-//	public void the_user_enters_the_valid_code_in_text_editor_and_clicks_run_button() {
-//	   
-//	    
-//	}
-
-//	@Then("The displayed in the console should be displayed")
-//	public void the_displayed_in_the_console_should_be_displayed() {
-//	   
-//	    
-//	}
-
-//	@When("The user enters the Invalid code in Text Editor and clicks RUN button")
-//	public void the_user_enters_the_invalid_code_in_text_editor_and_clicks_run_button() {
-//	   
-//	    
-//	}
-
-//	@Then("The nameerror pop up alert message should be displayed")
-//	public void the_nameerror_pop_up_alert_message_should_be_displayed() {
-//	   
-//	    
-//	}
-
-	@Given("The user is on the Try Editor of Graph page")
-	public void the_user_is_on_the_try_editor_of_graph_page() {
-	   
+	@Given("The user is on the tryEditor window in Graph page")
+	public void the_user_is_on_the_try_editor_window_in_graph_page() {
+		pageTitle = driver.getTitle();
+		Assert.assertEquals(pageTitle, "Assessment");
 	    
 	}
 
-	@When("The user clicks grabrowser back arrow")
-	public void the_user_clicks_grabrowser_back_arrow() {
-	   
+	@When("The user select Graph item from the drop down menu")
+	public void the_user_select_graph_item_from_the_drop_down_menu() {
 	    
-	}
+		graph.dropdown_graph_page();
 
-	@Then("The user is navigated to Graph page")
-	public void the_user_is_navigated_to_graph_page() {
-	   
-	    
 	}
 
 	@When("The user clicks the Graph Representations link in Graph Introduction page")
 	public void the_user_clicks_the_graph_representations_link_in_graph_introduction_page() {
-	   
+		graph.GraphRepresentations(); 
 	    
 	}
 
 	@Then("The user be directed to Graph Representations Page")
 	public void the_user_be_directed_to_graph_representations_page() {
-	   
+		   currentPageUrl = graph.getCurrentUrl();
+		   expectedPageUrl = "https://dsportalapp.herokuapp.com/graph/graph/";
+		   LoggerReader.info("The user is able to directed to graph page: "+currentPageUrl);
+		   Assert.assertEquals(expectedPageUrl, currentPageUrl,"The user is not directed to graph page");    	        
 	    
 	}
 
 	@Given("The user is in the Graph Representations page")
 	public void the_user_is_in_the_graph_representations_page() {
-	   
 	    
+		pageTitle = driver.getTitle();
+		Assert.assertEquals(pageTitle, "Graph Representations");
 	}
-
-	@Given("The user is on the GrareptryEditor page")
-	public void the_user_is_on_the_grareptry_editor_page() {
-	   
-	    
-	}
-
-	@Given("The user is on the Try Editor of Graph Representations page")
-	public void the_user_is_on_the_try_editor_of_graph_representations_page() {
-	   
-	    
-	}
-
-	@When("The user clicks grepbrowser back arrow")
-	public void the_user_clicks_grepbrowser_back_arrow() {
-	   
-	    
-	}
-
-	@Then("The user is navigated to Graph Representations page")
-	public void the_user_is_navigated_to_graph_representations_page() {
-	   
-	    
-	}
-
-	@Given("the user is signed in to dsAlgo Portal and is on the Graph page")
-	public void the_user_is_signed_in_to_ds_algo_portal_and_is_on_the_graph_page() {
-	   
-	    
-	}
-
-	@When("the user clicks the Practice Questions button")
-	public void the_user_clicks_the_practice_questions_button() {
-	   
-	    
-	}
-
-	@Then("the user should be redirected to the Practice page")
-	public void the_user_should_be_redirected_to_the_practice_page() {
-	   
-	    
-	}
-
-	@Then("The user is redirected to Graph Page")
-	public void the_user_is_redirected_to_graph_page() {
-	   
-	    
-	}
-
-//	@When("the user clicks Sign out")
-//	public void the_user_clicks_sign_out() {
-//	   
-//	    
-//	}
-
-	@Then("the user is directed to Home page with message Logged out successfully")
-	public void the_user_is_directed_to_home_page_with_message_logged_out_successfully() {
-	   
-	    
-	}
-
-
-
-
-
-
-
 
 }
--- src/test/resources/features/Graph.feature
+++ src/test/resources/features/Graph.feature
@@ -1,81 +1,87 @@
-Feature: Graph Module
- 
-   Background: The user logged in to dsAlgo Portal
-   Given The user is in the Home Page
+@tag
+Feature: Testing Graph feature in DSPortal app
+  User will be able to understand different topics and try python code
 
-@graph 
-Scenario: Navigate to Graph page
-Given The user is in the Graph Introduction Page after Sign in
-When The user clicks the  Graph link in Graph Introduction page
-Then The user be directed to Graph Page
+  @tag1
+  Scenario: Verify that user is able to navigate to "Graph" data structure page
+    Given The user is in the Home page after Sign in
+    When The user clicks the Get Started button in Graph Panel or The user select Graph item from the drop down menu
+    Then The user is directed to Graph Data Structure Page
 
-@graphtryhere
-Scenario: Navigate to Graph tryEditor window
-Given The user is in the Graph page
-When The user clicks Try here>>> button  
-Then The user is navigated to tryEditor window
+  @graph
+  Scenario: Navigate to Graph page
+    Given The user is in the Graph Introduction Page after Sign in
+    When The user clicks the Graph link in Graph Introduction page
+    Then The user be directed to Graph Page
 
-@graphtxteditor
-Scenario Outline: Graph tryEditor Functionality  
-Given The user is on the tryEditor page
-When The user enters the <code> in Text Editor and clicks RUN button
-Then The <alert> should be displayed
-Examples: 
-      | code          | alert                            |
-      | Empty         | Error message in alert window    |
-      | valid code    | displayed in the console         |
-      | Invalid code  | nameerror pop up alert message   |
-    
-@backtograph
-Scenario: Redirected to Graph page
-Given The user is on the Try Editor of Graph page
-When The user clicks browser back arrow
-Then The user is navigated to Graph page 
+  @graphtryhere
+  Scenario: Verify that user is able to navigate to try Editor page for Graph page
+    Given The user is in the Graph page
+    When The user clicks Try Here>>> button
+    Then Navigate to try Editor window with a Run button to test
 
-@grarep 
-Scenario: Navigate to Graph Representations page
-Given The user is in the Graph Introduction Page after Sign in
-When The user clicks the Graph Representations link in Graph Introduction page
-Then The user be directed to Graph Representations Page
+  @graphtxteditor
+  Scenario Outline: Graph tryEditor Functionality
+    Given The user is on the tryEditor window in Graph page
+    When The user enters the <code> in text editor and clicks RUN Button
+    Then The <alert> should be displayed
 
-@greptryhere
-Scenario: Navigate to Graph Representations tryEditor window
-Given The user is in the Graph Representations page
-When The user clicks Try here>>> button  
-Then The user is navigated to tryEditor window
+    Examples: 
+      | code         | alert                          |
+      | Empty        | Error message in alert window  |
+      | Valid code   | displayed in the console       |
+      | Invalid code | nameerror pop up alert message |
 
-@greptxteditor
-Scenario Outline: Graph Representations tryEditor Functionality  
-Given The user is on the tryEditor page
-When The user enters the <code> in Text Editor and clicks RUN button
-Then The <alert> should be displayed
-Examples: 
-      | code          | alert                            |
-      | Empty         | Error message in alert window    |
-      | valid code    | displayed in the console         |
-      | Invalid code  | nameerror pop up alert message   |
-    
-@backtogrep
-Scenario: Redirected to Graph Representations page
-Given The user is on the Try Editor of Graph Representations page
-When The user clicks browser back arrow
-Then The user is navigated to Graph Representations page 
+  @backtobinsea
+  Scenario: Navigate to "Practice Questions" Page for "Graph" page
+    Given The user is on the tryEditor window in Graph page
+    When The user clicks Practice Questions button following browser back arrow
+    Then The user is navigated to Practice Questions page
 
-@practques
-Scenario: Navigate to Practice Questions page for Graph
-Given the user is signed in to dsAlgo Portal and is on the Graph page
-When the user clicks the Practice Questions button
-Then the user should be redirected to the Practice page
+  @tag7
+  Scenario: Navigate to "Graph" data structure page
+    Given The user is in the blank Practice page
+    When The user select Graph item from the drop down menu
+    Then The user is directed to Graph Data Structure Page
 
-Scenario: Navigate to Graph page
-Given The user is in the blank Practice page
-When The user clicks browser back arrow
-Then The user is redirected to Graph Page
- 
-@signout
-Scenario: signout from Graph page and navigated to Home page
-Given The user is in the Graph page
-When The user clicks Sign out
-Then The user is directed to Home page with message Logged out successfully
-	
+  @grarep
+  Scenario: Navigate to Graph Representations page
+    Given The user is in the Graph Introduction Page after Sign in
+    When The user clicks the Graph Representations link in Graph Introduction page
+    Then The user be directed to Graph Representations Page
 
+  @greptryhere
+  Scenario: Navigate to Graph Representations tryEditor window
+    Given The user is in the Graph Representations page
+    When The user clicks Try Here>>> button
+    Then Navigate to try Editor window with a Run button to test
+
+  @greptxteditor
+  Scenario Outline: Graph Representations tryEditor Functionality
+    Given The user is on the tryEditor window in Graph page
+    When The user enters the <code> in text editor and clicks RUN Button
+    Then The <alert> should be displayed
+
+    Examples: 
+      | code         | alert                          |
+      | Empty        | Error message in alert window  |
+      | Valid code   | displayed in the console       |
+      | Invalid code | nameerror pop up alert message |
+
+  @practquesGraRep
+  Scenario: Navigate to "Practice Questions" Page for "Graph Representations" page
+    Given The user is on the tryEditor window in Graph page
+    When The user clicks Practice Questions button following browser back arrow
+    Then The user is navigated to Practice Questions page
+
+  @backtogra
+  Scenario: Navigate to Graph data structure page
+    Given The user is in the blank Practice page
+    When The user select Graph item from the drop down menu
+    Then The user is directed to Graph Data Structure Page
+
+  @signout
+  Scenario: signout from Graph page and navigated to Home page
+    Given The user is in the Graph page
+    When The user clicks Sign out
+    Then The user should be redirected to home page with message Logged out successfully

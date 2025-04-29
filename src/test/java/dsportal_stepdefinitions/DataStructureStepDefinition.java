bc4c0f47509ab9d0a15fb08445e7d1394633d014
bhoomi <bhoomi.chakrani@hotmail.com>
2025-04-28 22:43:13 -0400

    Update Data-structure files

--- src/test/java/dsportal_stepdefinitions/DataStructureStepDefinition.java
+++ src/test/java/dsportal_stepdefinitions/DataStructureStepDefinition.java
@@ -1,124 +1,86 @@
 package dsportal_stepdefinitions;
 
+
+import org.testng.Assert;
+
+import dsportal_DriverFactory.DriverManager;
+import dsportal_Page.DataStructurePage;
+import dsportal_Page.LoginPage;
+import dsportal_utilities.LoggerReader;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class DataStructureStepDefinition {
-	@Given("The user is in the Home page after logged in")
-	public void the_user_is_in_the_home_page_after_logged_in() {
-	    
-	   
+public class DataStructureStepDefinition extends DriverManager {
+	
+	static DataStructurePage DataStrucpge;
+	static LoginPage login_page;
+	String pageTitle, currentPageUrl, expectedPageUrl ;
+	String homePageURL = "https://dsportalapp.herokuapp.com/";
+	String dsPageURL = homePageURL+"data-structures-introduction/";
+	
+	public static void setUpDataStructurePage() {
+		DataStrucpge = new DataStructurePage(driver);
 	}
-
-	@When("The user clicks the Getting Started button in Data Structures - Introduction OR The user select Data Structures - Introduction item from the drop down menu")
-	public void the_user_clicks_the_getting_started_button_in_data_structures_introduction_or_the_user_select_data_structures_introduction_item_from_the_drop_down_menu() {
-	    
-	   
+	
+	@When("The user clicks the Getting Started button in Data Structures - Introduction")
+	public void the_user_clicks_the_getting_started_button_in_data_structures_introduction() {
+		if(DataStrucpge==null) {
+  			LoggerReader.info("Create constructor for Data Structures page");
+  			setUpDataStructurePage();
+  		}
+	   DataStrucpge.DataStruc_page(); 
 	}
 
 	@Then("The user be directed to Data Structures- Introduction Page")
 	public void the_user_be_directed_to_data_structures_introduction_page() {
+		   currentPageUrl = DataStrucpge.getCurrentUrl();
+		   expectedPageUrl = dsPageURL;
+		   LoggerReader.info("The user is able to directed to Data Structures page: "+currentPageUrl);
+		   Assert.assertEquals(expectedPageUrl, currentPageUrl,"The user is not directed to Data Structures page"); 
 	    
-	   
 	}
 
 	@Given("The user is in the Data Structures - Introduction page")
 	public void the_user_is_in_the_data_structures_introduction_page() {
+		pageTitle =DataStrucpge.getCurrentTitle();
+		Assert.assertEquals(pageTitle, "Data Structures-Introduction");
 	    
-	   
 	}
 
 	@When("The user clicks Time Complexity button")
 	public void the_user_clicks_time_complexity_button() {
+		DataStrucpge.TimecomplexInDataStruc_page();
 	    
-	   
 	}
 
 	@Then("The user should be redirected to Time Complexity of Data structures-Introduction")
 	public void the_user_should_be_redirected_to_time_complexity_of_data_structures_introduction() {
+		   currentPageUrl = DataStrucpge.getCurrentUrl();
+		   expectedPageUrl = "https://dsportalapp.herokuapp.com/data-structures-introduction/time-complexity/";
+		   LoggerReader.info("The user is able to directed to Time Complexity page: "+currentPageUrl);
+		   Assert.assertEquals(currentPageUrl, expectedPageUrl, "The user is not directed to Time Complexity page"); 
 	    
-	   
 	}
 
 	@Given("The user is in the Time Complexity page")
 	public void the_user_is_in_the_time_complexity_page() {
+		pageTitle = DataStrucpge.getCurrentTitle();
+		Assert.assertEquals(pageTitle, "Time Complexity");
 	    
-	   
 	}
 
-	@When("The user clicks Try Here button")
-	public void the_user_clicks_try_here_button() {
+	@Given("The user is on the tryEditor Operations in Data structures-Introduction page")
+	public void the_user_is_on_the_try_editor_operations_in_data_structures_introduction_page() {
+		pageTitle = DataStrucpge.getCurrentTitle();
+		Assert.assertEquals(pageTitle, "Assessment");     
 	    
-	   
 	}
 
-	@Then("The user should be redirected to a page having an try Editor with a Run button to test")
-	public void the_user_should_be_redirected_to_a_page_having_an_try_editor_with_a_run_button_to_test() {
-	    
-	   
+	@When("The user clicks browser back arrow to go to Data Structures - Introduction  page")
+	public void the_user_clicks_browser_back_arrow_to_go_to_data_structures_introduction_page() {
+		driver.navigate().back();
+		driver.navigate().back();
 	}
-
-	@Given("The user is in the tryEditor page")
-	public void the_user_is_in_the_try_editor_page() {
-	    
-	   
-	}
-
-	@When("The user clicks the Run Button without entering the code in the Editor")
-	public void the_user_clicks_the_run_button_without_entering_the_code_in_the_editor() {
-	    
-	   
-	}
-
-	@Then("The user should able to see an error message in alert window")
-	public void the_user_should_able_to_see_an_error_message_in_alert_window() {
-	    
-	   
-	}
-
-	@When("The user write the invalid code in Editor and click the Run Button")
-	public void the_user_write_the_invalid_code_in_editor_and_click_the_run_button() {
-	    
-	   
-	}
-
-	@When("The user write the valid code in Editor and click the Run Button")
-	public void the_user_write_the_valid_code_in_editor_and_click_the_run_button() {
-	    
-	   
-	}
-
-	@Then("The user should able to see output in the console")
-	public void the_user_should_able_to_see_output_in_the_console() {
-	    
-	   
-	}
-
-	@When("The user clicks the Practice Questions button")
-	public void the_user_clicks_the_practice_questions_button() {
-	    
-	   
-	}
-
-	@Then("The user should be redirected to Practice Questions of Data structures-Introduction")
-	public void the_user_should_be_redirected_to_practice_questions_of_data_structures_introduction() {
-	    
-	   
-	}
-
-	@When("The user selected item from the drop down menu")
-	public void the_user_selected_item_from_the_drop_down_menu() {
-	    
-	   
-	}
-
-	@Then("The user be directed to selected itemPage")
-	public void the_user_be_directed_to_selected_item_page() {
-	    
-	   
-	}
-
-	
-
 }
+
